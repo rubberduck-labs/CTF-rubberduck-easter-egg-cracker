5a82f30ea4cc48b8238637839b4c714f032660d9
Simen Jensen <simen-jarl.jensen@telenor.com>
2023-04-02 00:01:33 +0200

    Test excluding file

--- api/easter_egg.ts
+++ api/easter_egg.ts
@@ -5,11 +5,9 @@ import path from 'path';
 export default async function(req: VercelRequest, res: VercelResponse) {
   const secret = req.query.secret;
 
-  await new Promise((aa) => setTimeout(aa, 6000));
-
   if (secret === process.env.EASTER_EGG_FLAG) {
-    const file = readFileSync(path.join(process.cwd(), 'api_resources', 'reward.jpeg'));
-    res.setHeader('conent-type', 'image/jpeg');
+    const file = readFileSync(path.join(process.cwd(), 'api_resources', 'easter_egg.png'));
+    res.setHeader('conent-type', 'image/png');
     res.end(file);
   } else {
     return res.status(403).json({ error: 'Forbidden' });
