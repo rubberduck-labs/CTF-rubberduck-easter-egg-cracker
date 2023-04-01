7809e2c03c834ab2afcf2a37290934ba48c0fbcc
Simen Jensen <simen-jarl.jensen@telenor.com>
2023-04-02 00:04:42 +0200

    Test including folder

--- api/easter_egg.ts
+++ api/easter_egg.ts
@@ -1,14 +1,17 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { readFileSync } from 'fs';
+import fs from 'fs';
 import path from 'path';
 
 export default async function(req: VercelRequest, res: VercelResponse) {
   const secret = req.query.secret;
 
   if (secret === process.env.EASTER_EGG_FLAG) {
-    const file = readFileSync(path.join(process.cwd(), 'api_resources', 'easter_egg.png'));
-    res.setHeader('conent-type', 'image/png');
-    res.end(file);
+    // const file = readFileSync(path.join(process.cwd(), 'api_resources', 'easter_egg.png'));
+    // res.setHeader('conent-type', 'image/png');
+    // res.end(file);
+
+    const possibleFiles = fs.readdirSync(process.cwd());
+    return res.json(possibleFiles);
   } else {
     return res.status(403).json({ error: 'Forbidden' });
   }
