8293b677055b7f076f480a9bc8b653b77d7528ea
Simen Jensen <sj@rubberduck.no>
2023-04-02 02:22:16 +0200

    Fix minified easter egg function

--- src/assets/minified/easter.egg.js
+++ src/assets/minified/easter.egg.js
@@ -1,39 +1,39 @@
-const _0x769dd6 = _0x2400;
-(function (_0x115b32, _0x1cb9de) {
-  const _0x7ae5d9 = _0x2400, _0xb04e8e = _0x115b32();
+function _0xe90f(_0x3c1063, _0x5cf6b4) {
+  const _0x23c61f = _0x23c6();
+  return _0xe90f = function (_0xe90fca, _0x483aa6) {
+    _0xe90fca = _0xe90fca - 438;
+    let _0x2893e8 = _0x23c61f[_0xe90fca];
+    return _0x2893e8;
+  }, _0xe90f(_0x3c1063, _0x5cf6b4);
+}
+const _0x3c6f20 = _0xe90f;
+(function (_0x74ea5a, _0x3b4905) {
+  const _0x1836b2 = _0xe90f, _0x2b06f0 = _0x74ea5a();
   while (true) {
     try {
-      const _0x11ec36 = parseInt(_0x7ae5d9(205)) / 1 * (parseInt(_0x7ae5d9(176)) / 2) + parseInt(_0x7ae5d9(216)) / 3 * (parseInt(_0x7ae5d9(170)) / 4) + -parseInt(_0x7ae5d9(191)) / 5 * (-parseInt(_0x7ae5d9(204)) / 6) + -parseInt(_0x7ae5d9(231)) / 7 * (parseInt(_0x7ae5d9(189)) / 8) + -parseInt(_0x7ae5d9(237)) / 9 * (-parseInt(_0x7ae5d9(203)) / 10) + parseInt(_0x7ae5d9(223)) / 11 * (parseInt(_0x7ae5d9(228)) / 12) + -parseInt(_0x7ae5d9(226)) / 13;
-      if (_0x11ec36 === _0x1cb9de) break; else _0xb04e8e.push(_0xb04e8e.shift());
-    } catch (_0x12ac4d) {
-      _0xb04e8e.push(_0xb04e8e.shift());
+      const _0x4e671c = -parseInt(_0x1836b2(456)) / 1 * (parseInt(_0x1836b2(438)) / 2) + parseInt(_0x1836b2(439)) / 3 * (-parseInt(_0x1836b2(457)) / 4) + parseInt(_0x1836b2(446)) / 5 + parseInt(_0x1836b2(443)) / 6 + -parseInt(_0x1836b2(453)) / 7 + parseInt(_0x1836b2(447)) / 8 + -parseInt(_0x1836b2(450)) / 9 * (-parseInt(_0x1836b2(458)) / 10);
+      if (_0x4e671c === _0x3b4905) break; else _0x2b06f0.push(_0x2b06f0.shift());
+    } catch (_0x1a6fcd) {
+      _0x2b06f0.push(_0x2b06f0.shift());
     }
   }
-}(_0x1dde, 386125));
-let currentPath = "./";
-const recognizedCommands = {_0x249c52: _0xdea57a => fetch(_0x769dd6(169) + _0x769dd6(209) + "omm" + _0x769dd6(174) + "_li" + _0x769dd6(195) + _0x769dd6(214) + _0x769dd6(207) + _0x769dd6(179) + (currentPath + (_0xdea57a || "")))[_0x769dd6(200) + "n"](_0x5908c9 => _0x5908c9.ok ? _0x5908c9[_0x769dd6(188) + "t"]() : "<sp" + _0x769dd6(175) + _0x769dd6(186) + _0x769dd6(208) + _0x769dd6(193) + _0x769dd6(220) + 'id"' + _0x769dd6(232) + _0x769dd6(213) + "d t" + _0x769dd6(215) + _0x769dd6(173) + _0x769dd6(236) + "mma" + _0x769dd6(197) + "/sp" + _0x769dd6(235)), _0x3f644e: _0x4bff2b => fetch("/ap" + _0x769dd6(209) + _0x769dd6(234) + _0x769dd6(174) + _0x769dd6(192) + _0x769dd6(195) + _0x769dd6(238) + _0x769dd6(194) + _0x769dd6(217) + "m=" + (_0x4bff2b || ""))[_0x769dd6(200) + "n"](_0x570ed7 => _0x570ed7.ok ? _0x570ed7[_0x769dd6(188) + "t"]() : _0x769dd6(211) + _0x769dd6(175) + "cla" + _0x769dd6(208) + _0x769dd6(193) + _0x769dd6(220) + _0x769dd6(172) + _0x769dd6(232) + _0x769dd6(213) + _0x769dd6(206) + _0x769dd6(215) + _0x769dd6(173) + _0x769dd6(236) + "mma" + _0x769dd6(197) + _0x769dd6(233) + _0x769dd6(235)), _0x15757c: _0x3943aa => fetch(_0x769dd6(169) + _0x769dd6(209) + _0x769dd6(234) + _0x769dd6(174) + _0x769dd6(192) + _0x769dd6(195) + _0x769dd6(221) + _0x769dd6(222) + _0x769dd6(177) + "=" + (currentPath + (_0x3943aa || "")))[_0x769dd6(200) + "n"](_0x31f71f => _0x31f71f.ok ? _0x31f71f[_0x769dd6(188) + "t"]() : _0x769dd6(211) + _0x769dd6(175) + "cla" + _0x769dd6(208) + '"in' + _0x769dd6(220) + _0x769dd6(172) + _0x769dd6(232) + "ile" + _0x769dd6(206) + _0x769dd6(215) + "all" + _0x769dd6(236) + "mma" + _0x769dd6(197) + "/sp" + _0x769dd6(235)), _0x271787: _0x13ad60 => fetch(_0x769dd6(169) + _0x769dd6(209) + _0x769dd6(234) + "and" + _0x769dd6(192) + _0x769dd6(195) + _0x769dd6(187) + "der" + _0x769dd6(222) + "ram" + "=" + (currentPath + (_0x13ad60 || "")))[_0x769dd6(200) + "n"](_0x565695 => _0x565695.ok ? _0x565695[_0x769dd6(188) + "t"]() : "<sp" + _0x769dd6(175) + _0x769dd6(186) + _0x769dd6(208) + _0x769dd6(193) + _0x769dd6(220) + _0x769dd6(172) + ">fa" + _0x769dd6(213) + "d t" + _0x769dd6(215) + _0x769dd6(173) + _0x769dd6(236) + _0x769dd6(183) + "nd<" + "/sp" + "an>")};
-function _0x2400(_0x2805a3, _0x4b80d3) {
-  const _0x1dde9a = _0x1dde();
-  return _0x2400 = function (_0x240053, _0x20b3ce) {
-    _0x240053 = _0x240053 - 169;
-    let _0x188ae0 = _0x1dde9a[_0x240053];
-    return _0x188ae0;
-  }, _0x2400(_0x2805a3, _0x4b80d3);
-}
-function _0x1dde() {
-  const _0x440b44 = ["p?p", "ne/", "sto", "nd<", "Wri", "k-c", "the", "ion", "see", "373260AJqUAh", "1177986OXZtCC", "27625LuIafQ", "d t", "par", "ss=", "i/c", "Eve", "<sp", "4cc", "ile", "ls?", "o c", "3UoNIee", "ara", "ail", "_0x", "val", "cat", "?pa", "4642cCmWYW", "opa", "get", "9524385WtPcWH", "gat", "15828MzCEbY", "cle", "det", "224RixkJB", ">fa", "/sp", "omm", "an>", " co", "9BhCyqL", "hel", "ntL", "/ap", "1381912HVacpB", "bod", 'id"', "all", "and", "an ", "2VxscqC", "ram", "pPr", "am=", "TML", "ene", "ter", "mma", "ist", "add", "cla", "ren", "tex", "11152XTQyat", "293", "5CgGDUA", "_li", '"in'];
-  _0x1dde = function () {
-    return _0x440b44;
+}(_0x23c6, 704250));
+function _0x23c6() {
+  const _0x44cf15 = ["clear", "7339944wFSEZW", "addEventListener", "seek-command", "4000020oiaeLG", "8907936wDgmRE", '<span class="invalid">failed to call command</span>', "run", "1718613oDrZED", "then", "/api/command_line/render?param=", "8954540kpGAcq", "commands", "text", "3xtEOVX", "4qPxxXr", "20DYgDKW", "726502CWoRyk", "1336533mBEvSF", "/api/command_line/cat?param=", "stopPropagation"];
+  _0x23c6 = function () {
+    return _0x44cf15;
   };
-  return _0x1dde();
+  return _0x23c6();
 }
+let currentPath = "./";
+const recognizedCommands = {ls: _0x4fd252 => fetch("/api/command_line/ls?param=" + (currentPath + (_0x4fd252 || "")))[_0x3c6f20(451)](_0x28c268 => _0x28c268.ok ? _0x28c268[_0x3c6f20(455)]() : '<span class="invalid">failed to call command</span>'), help: _0x57eccf => fetch("/api/command_line/help?param=" + (_0x57eccf || ""))[_0x3c6f20(451)](_0x4dfbee => _0x4dfbee.ok ? _0x4dfbee[_0x3c6f20(455)]() : _0x3c6f20(448)), cat: _0x477e0a => fetch(_0x3c6f20(440) + (currentPath + (_0x477e0a || "")))[_0x3c6f20(451)](_0x55ebfe => _0x55ebfe.ok ? _0x55ebfe[_0x3c6f20(455)]() : '<span class="invalid">failed to call command</span>'), render: _0x2c0283 => fetch(_0x3c6f20(452) + (currentPath + (_0x2c0283 || "")))[_0x3c6f20(451)](_0x3b6274 => _0x3b6274.ok ? _0x3b6274[_0x3c6f20(455)]() : _0x3c6f20(448))};
 (function () {
-  const _0x1a1d8b = _0x769dd6;
-  document[_0x1a1d8b(171) + "y"][_0x1a1d8b(185) + _0x1a1d8b(210) + _0x1a1d8b(239) + _0x1a1d8b(184) + _0x1a1d8b(181) + "r"](_0x1a1d8b(202) + _0x1a1d8b(199) + _0x1a1d8b(234) + _0x1a1d8b(174), _0x18727a => {
-    const _0x4746af = _0x1a1d8b, {command: _0x3f1e3c, _0xab353: _0x341fd3} = _0x18727a[_0x4746af(230) + _0x4746af(218)], [_0x14f656, _0xa7c0a8] = _0x3f1e3c.split(" ");
-    !!recognizedCommands[_0x14f656] && (_0x18727a[_0x4746af(196) + _0x4746af(178) + _0x4746af(224) + _0x4746af(227) + _0x4746af(201)](), _0x341fd3[_0x4746af(219) + _0x4746af(212) + "f94"] = async _0x251fa4 => {
-      const _0x37287e = _0x4746af, _0x5a9635 = _0x251fa4[_0x37287e(225) + _0x37287e(198) + _0x37287e(182)]();
-      await _0x5a9635(await recognizedCommands[_0x14f656](_0xa7c0a8));
-    }), _0x14f656 === _0x4746af(229) + "ar" && (_0x18727a["sto" + _0x4746af(178) + _0x4746af(224) + _0x4746af(227) + _0x4746af(201)](), _0x341fd3[_0x4746af(219) + "5cd" + _0x4746af(190)]["innerH" + _0x4746af(180)] = ""), _0x14f656 === "cd" && (_0x18727a[_0x4746af(196) + "pPr" + _0x4746af(224) + _0x4746af(227) + "ion"](), currentPath += _0xa7c0a8 + "/");
+  const _0x2869cd = _0x3c6f20;
+  document.body[_0x2869cd(444)](_0x2869cd(445), _0x27db3d => {
+    const _0x9ecbd1 = _0x2869cd, {command: _0x42828d, terminal: _0x2e0ea4} = _0x27db3d.detail, [_0x259c83, _0x29937e] = _0x42828d.split(" ");
+    !!recognizedCommands[_0x259c83] && (_0x27db3d[_0x9ecbd1(441)](), _0x2e0ea4[_0x9ecbd1(449)] = async _0x18e788 => {
+      const _0x1cfca1 = _0x18e788.getWriter();
+      await _0x1cfca1(await recognizedCommands[_0x259c83](_0x29937e));
+    }), _0x259c83 === _0x9ecbd1(442) && (_0x27db3d[_0x9ecbd1(441)](), _0x2e0ea4[_0x9ecbd1(454)].innerHTML = ""), _0x259c83 === "cd" && (_0x27db3d[_0x9ecbd1(441)](), currentPath += _0x29937e + "/");
   });
 }());
